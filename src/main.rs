3f57bf622cd28a1296da7d79c45036ee6c8ffb4b
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-26 15:26:41 +0000

    att

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 fn main() {
-
     let greet: &str = "Hello, Pablo!";
 
     let dolar_price: f64 = 6.10;
-    let euroPrice: f64 = 6.20;  // never use camelCase 
+    let euro_price: f64 = 6.20;  // Snake case
 
     let user_1: &str = "First User";
 
     let var_with_a_big_name: &str = "Variavel";
 
-    println!("{}", greet);
+    let x = (123, "Pablo");
+
+    // Exemplo de uso das variáveis para evitar warnings
+    println!("Greetings: {}", greet);
+    println!("Dollar price: ${}\nEuro price: €{}", dolar_price, euro_price);
+    println!("User: {}\nBig name variable: {}", user_1, var_with_a_big_name);
+    println!("Tuple: {:?}", x);
 }
