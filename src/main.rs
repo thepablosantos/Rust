004fe635dd7e3e5d650291ea65adcbc453a07235
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-26 14:38:54 +0000

    var

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ fn main() {
 
     let greet: &str = "Hello, Pablo!";
 
+    let dolar_price: f64 = 6.10;
+    let euroPrice: f64 = 6.20;  // never use camelCase 
+
+    let user_1: &str = "First User";
+
     println!("{}", greet);
 }
