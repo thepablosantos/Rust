f2ed1df4dd49680e9fcc85af1ad874fec90a1a8d
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-28 21:36:17 +0000

    att

--- src/logic.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let age: i32 = 26; 
-
-    if age > 18 {
-        println!("Qual cerveja você deseja?");
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 fn main() {
-    let greet: &str = "Hello, Pablo!";
+    let age: i32 = 26;
+    let mut result: bool = age > 18;
+    result: bool = age < 18;
+    result: bool = age >= 18;
+    result: bool = age == 18;
+    result: bool = age != 18;
 
-    let dolar_price: f64 = 6.10;
-    let euro_price: f64 = 6.20;  // Snake case
-
-    let user_1: &str = "First User";
-
-    let var_with_a_big_name: &str = "Variavel";
-
-    let x = (123, "Pablo");
-
-    // Exemplo de uso das variáveis para evitar warnings
-    println!("Greetings: {}", greet);
-    println!("Dollar price: ${}\nEuro price: €{}", dolar_price, euro_price);
-    println!("User: {}\nBig name variable: {}", user_1, var_with_a_big_name);
-    println!("Tuple: {:?}", x);
-} //
+    if result > 18 {
+        println!("Qual cerveja você deseja?");
+    } else {
+        println!("Não podemos vender para menor");
+    }
+}
