eb16de8f1b926f2bb89656bb7b1552ebff00d651
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-29 11:09:00 +0000

    att

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    show_number(320);
-    show_number(320);
-    show_number(320);
+    let z: i32 = process_number (90, 13);
+    print!("{z}");
 }
 
-fn show_number(number: u32) {
-
-    println!("{number}");
+fn process_number(x: i32, y: i32) -> i32 {
+    x + y
 }
-
